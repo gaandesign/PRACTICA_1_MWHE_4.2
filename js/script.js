8dc90f8059ee6d603464dd4c59ea7878c23ca330
saralishu <saralishu98@gmail.com>
2024-10-17 13:29:42 +0200

    add seccion premium
    Esta en proceso que me estoy peleando con la estrella giratoria jejeje :(

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 // console.log("online");
+
+
+const rotatingSvg = document.getElementById('rotating-svg');
+
+window.addEventListener('scroll', () => {
+    const scrollY = window.scrollY;
+    const rotation = scrollY * 0.5; // Cambia el 0.5 para ajustar la velocidad de rotación
+    rotatingSvg.style.transform = `translateX(-50%) rotate(${rotation}deg)`;
+});
