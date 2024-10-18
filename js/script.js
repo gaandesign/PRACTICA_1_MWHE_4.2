b0ce5fcaca377530d6f01d489e2b43b8856261d3
gaandesign <marinagarridoandres@gmail.com>
2024-10-18 19:33:12 +0200

    menu final + cookie pop up

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ window.addEventListener('scroll', () => {
     const rotation = scrollY * 0.5; // Cambia el 0.5 para ajustar la velocidad de rotación
     rotatingSvg.style.transform = `translateX(-50%) rotate(${rotation}deg)`;
 });
+
+
+$(document).ready(function(){
+
+    // POP UP
+
+    // Muestra el pop-up de cookies después de 2 segundos
+    setTimeout(function(){
+      $("#cookie-popup").fadeIn();
+    }, 2000);
+
+    // Maneja el clic en el botón de aceptar cookies
+    $("#accept-cookies").click(function(){
+        $("#cookie-popup").fadeOut();
+    });   
+
+});
